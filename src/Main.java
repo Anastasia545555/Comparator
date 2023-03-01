7be8ba436880ff80b0b9707934d556f17d3ce5ec
Anastasiya <lazycat73332@gmail.com>
2023-03-01 22:31:51 +0300

    Класс PersonComparator.java заменён на компаратор

--- src/Main.java
+++ src/Main.java
@@ -14,7 +14,14 @@ public class Main{
             System.out.println(p.toString());
         }
         System.out.println("Список после сортировки по возрастанию знатности: ");
-        Collections.sort(pl, new PersonComparator());
+        Comparator<Person> comparator= (p1, p2) ->{
+            Set<String> p1_set=new HashSet<>(List.of(p1.getSurname().split("\\P{IsAlphabetic}+")));
+            Set<String> p2_set=new HashSet<>(List.of(p2.getSurname().split("\\P{IsAlphabetic}+")));
+            if (p1_set.size()<p2_set.size()) return -1;
+            else if ((p1_set.size()==p2_set.size())) return Integer.compare(p1.getAge(), p2.getAge());
+            return 1;
+        };
+        Collections.sort(pl, comparator);
         for (Person p: pl){
             System.out.println(p.toString());
         }
