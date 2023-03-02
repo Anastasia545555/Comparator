ae9e0b883e520d41a6051f385e9c03287618a00c
Anastasiya <lazycat73332@gmail.com>
2023-03-02 10:16:11 +0300

    Реализовано удаление людей, не достигших 18 лет через предикат. Вывод списка выделен в отдельный метод

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.function.Predicate;
 
 public class Main{
     public static void main(String[] args) {
@@ -9,12 +10,19 @@ public class Main{
         pl.add(new Person("Иван", "Иванов Петров Андреев", 17));
         pl.add(new Person("Богдан", "Воронов Грачев Соколов Соловьёв", 24));
         pl.add(new Person("Ольга", "Белова Краснова", 22));
+        pl.add(new Person("Олег", "Львов", 12));
         System.out.println("Список до сортировки: ");
-        for (Person p: pl){
-            System.out.println(p.toString());
-        }
+        printList(pl);
         System.out.println("Список после сортировки по возрастанию знатности: ");
         Collections.sort(pl, new PersonComparator());
+        printList(pl);
+        System.out.println("Список после судаления людей, не достигших 18-летия: ");
+        Predicate<Person> t = person -> person.getAge()<18;
+        pl.removeIf(t);
+        printList(pl);
+    }
+
+    public static void printList(List<Person> pl) {
         for (Person p: pl){
             System.out.println(p.toString());
         }
